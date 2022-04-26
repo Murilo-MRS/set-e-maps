7a167eb4fc3b0e1cae6272bd11e27c3f0123d0b5
Murilo-MRS <murilosantana1993@gmail.com>
2022-04-26 19:44:50 -0300

    Exercicio 2 de reduce

---
+++ map-filter-reduce/playground-reduce2.js
@@ -0,0 +1,24 @@
+const lista = [
+    {
+        name: 'sabao em po',
+        preco: 30,
+    },
+    {
+        name: 'cereal',
+        preco: 12,
+    },
+    {
+        name: 'toalha',
+        preco: 30,
+    },
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista) {
+    return lista.reduce(function (prev, current) {
+        return prev - current.preco;
+    }, saldoDisponivel);
+}
+
+console.log(calculaSaldo(saldoDisponivel, lista));
